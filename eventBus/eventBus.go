acf206e2975431a3457cc0de6584e026812cda5b
gwokwong <guowang1102@163.com>
2023-07-04 13:40:16 +0800

    feat:优化锁

--- eventBus/eventBus.go
+++ eventBus/eventBus.go
@@ -4,38 +4,45 @@ import (
 	"sync"
 )
 
+// EventID 定义事件ID类型
 type EventID string
 
+// Event 定义事件接口
 type Event interface {
 	EventID() EventID
 }
 
+// EventHandler 定义事件处理函数类型
 type EventHandler func(event Event)
 
+// Subscription 定义订阅对象
 type Subscription struct {
 	eventID EventID
 	id      uint64
 }
 
+// BusSubscriber 定义事件订阅者接口
 type BusSubscriber interface {
 	Subscribe(eventID EventID, cb EventHandler) Subscription
-	Unsubscribe(id Subscription)
+	Unsubscribe(sub Subscription)
 }
 
+// BusPublisher 定义事件发布者接口
 type BusPublisher interface {
 	Publish(event Event)
 }
 
+// Bus 定义事件总线接口
 type Bus interface {
 	BusSubscriber
 	BusPublisher
 }
 
-func New() Bus {
-	b := &bus{
-		infos: make(map[EventID]subscriptionInfoList),
-	}
-	return b
+// bus 实现了 Bus 接口
+type bus struct {
+	lock   sync.Mutex
+	nextID uint64
+	infos  map[EventID][]*subscriptionInfo
 }
 
 type subscriptionInfo struct {
@@ -43,61 +50,72 @@ type subscriptionInfo struct {
 	cb EventHandler
 }
 
-type subscriptionInfoList []*subscriptionInfo
-
-type bus struct {
-	lock   sync.Mutex
-	nextID uint64
-	infos  map[EventID]subscriptionInfoList
+// New 创建一个新的事件总线
+func New() Bus {
+	return &bus{
+		infos: make(map[EventID][]*subscriptionInfo),
+	}
 }
 
-func (bus *bus) Subscribe(eventID EventID, cb EventHandler) Subscription {
-	bus.lock.Lock()
-	defer bus.lock.Unlock()
-	id := bus.nextID
-	bus.nextID++
+func (b *bus) Subscribe(eventID EventID, cb EventHandler) Subscription {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	id := b.nextID
+	b.nextID++
+
 	sub := &subscriptionInfo{
 		id: id,
 		cb: cb,
 	}
-	bus.infos[eventID] = append(bus.infos[eventID], sub)
+	b.infos[eventID] = append(b.infos[eventID], sub)
+
 	return Subscription{
 		eventID: eventID,
 		id:      id,
 	}
 }
 
-func (bus *bus) Unsubscribe(subscription Subscription) {
-	bus.lock.Lock()
-	defer bus.lock.Unlock()
+func (b *bus) Unsubscribe(subscription Subscription) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
 
-	if infos, ok := bus.infos[subscription.eventID]; ok {
+	if infos, ok := b.infos[subscription.eventID]; ok {
 		for idx, info := range infos {
 			if info.id == subscription.id {
 				infos = append(infos[:idx], infos[idx+1:]...)
 				break
 			}
 		}
+
 		if len(infos) == 0 {
-			delete(bus.infos, subscription.eventID)
+			delete(b.infos, subscription.eventID)
 		} else {
-			bus.infos[subscription.eventID] = infos
+			b.infos[subscription.eventID] = infos
 		}
 	}
 }
 
-func (bus *bus) Publish(event Event) {
-	infos := bus.copySubscriptions(event.EventID())
+func (b *bus) Publish(event Event) {
+	infos := b.copySubscriptions(event.EventID())
+
 	for _, sub := range infos {
 		sub.cb(event)
 	}
 }
 
-func (bus *bus) copySubscriptions(eventID EventID) subscriptionInfoList {
-	bus.lock.Lock()
-	defer bus.lock.Unlock()
-	if infos, ok := bus.infos[eventID]; ok {
-		return infos
+func (b *bus) copySubscriptions(eventID EventID) []*subscriptionInfo {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	infos, ok := b.infos[eventID]
+	if !ok {
+		return nil
 	}
-	return subscriptionInfoList{}
+
+	// 复制订阅列表，以避免在迭代时修改原始列表
+	copiedInfos := make([]*subscriptionInfo, len(infos))
+	copy(copiedInfos, infos)
+
+	return copiedInfos
 }
