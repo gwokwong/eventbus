76a294c1b187477768a0a8dec394a8cfda8ac00d
gwok <guowang1102@163.com>
2023-07-28 14:12:29 +0800

    并发执行发布

--- eventbus.go
+++ eventbus.go
@@ -11,57 +11,60 @@ type Event struct {
 	Payload interface{}
 }
 
+// EventHandler 定义处理事件的处理函数类型
 type EventHandler func(event Event)
 
+// Subscriber 订阅者结构体
 type Subscriber struct {
 	handler EventHandler
 }
 
+// EventBus 事件总线
 type EventBus struct {
-	subscribers map[string][]*Subscriber
-	mu          sync.RWMutex
+	subscribers sync.Map
 }
 
-// NewEventBus 新建事件
+// NewEventBus 新建事件总线
 func NewEventBus() *EventBus {
-	return &EventBus{
-		subscribers: make(map[string][]*Subscriber),
-	}
+	return &EventBus{}
 }
 
-// Subscribe 订阅
+// Subscribe 订阅事件
 func (eb *EventBus) Subscribe(eventName string, handler EventHandler) {
-	eb.mu.Lock()
-	defer eb.mu.Unlock()
-	if _, ok := eb.subscribers[eventName]; !ok {
-		eb.subscribers[eventName] = []*Subscriber{}
-	}
+	subscribers, _ := eb.subscribers.LoadOrStore(eventName, []*Subscriber{})
 	subscriber := &Subscriber{handler: handler}
-	eb.subscribers[eventName] = append(eb.subscribers[eventName], subscriber)
+	eb.subscribers.Store(eventName, append(subscribers.([]*Subscriber), subscriber))
 }
 
 // Unsubscribe 取消订阅
 func (eb *EventBus) Unsubscribe(eventName string, handler EventHandler) {
-	eb.mu.Lock()
-	defer eb.mu.Unlock()
-	if subscribers, ok := eb.subscribers[eventName]; ok {
-		for i, subscriber := range subscribers {
-			if reflect.ValueOf(subscriber.handler).Pointer() == reflect.ValueOf(handler).Pointer() {
-				// Remove the subscriber from the subscribers list
-				eb.subscribers[eventName] = append(subscribers[:i], subscribers[i+1:]...)
-				break
+	if subscribers, ok := eb.subscribers.Load(eventName); ok {
+		oldSubscribers := subscribers.([]*Subscriber)
+		newSubscribers := make([]*Subscriber, 0, len(oldSubscribers))
+		for _, subscriber := range oldSubscribers {
+			if reflect.ValueOf(subscriber.handler).Pointer() != reflect.ValueOf(handler).Pointer() {
+				newSubscribers = append(newSubscribers, subscriber)
 			}
+		}
+		if len(newSubscribers) == 0 {
+			eb.subscribers.Delete(eventName)
+		} else {
+			eb.subscribers.Store(eventName, newSubscribers)
 		}
 	}
 }
 
-// Publish 发布
+// Publish 发布事件
 func (eb *EventBus) Publish(event Event) {
-	eb.mu.RLock()
-	defer eb.mu.RUnlock()
-	if subscribers, ok := eb.subscribers[event.Name]; ok {
-		for _, subscriber := range subscribers {
-			subscriber.handler(event)
+	wg := sync.WaitGroup{}
+	if subscribers, ok := eb.subscribers.Load(event.Name); ok {
+		for _, subscriber := range subscribers.([]*Subscriber) {
+			wg.Add(1)
+			go func(subscriber *Subscriber) {
+				defer wg.Done()
+				subscriber.handler(event)
+			}(subscriber)
 		}
 	}
+	wg.Wait()
 }
